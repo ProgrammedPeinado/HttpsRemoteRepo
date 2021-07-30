308ed307f36034167d1945d4bdc375250e570582
Hector Alarcon <hectorpeinado.luis@gmail.com>
2021-07-30 15:13:26 -0400

    Story 2 - First option added with functionality to check root,files and folders in it

--- Simplilearn/Final Projects/Implement OOPS using JAVA with Data Structures and Beyond/src/lockedmeapp/LockedMe.java
+++ Simplilearn/Final Projects/Implement OOPS using JAVA with Data Structures and Beyond/src/lockedmeapp/LockedMe.java
@@ -1,11 +1,18 @@
 package lockedmeapp;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LockedMe 
 {
-	private static Scanner input = new Scanner(System.in);
+	private static Scanner input = new Scanner(System.in);	
 	private static String sel;
+	private static String path = Paths.get("").toAbsolutePath().toString();
+	private static final String USER_STORAGE = "\\Implement OOPS using JAVA with Data Structures and Beyond\\src\\ApplicationStorage";
+	private static File storage = new File(path+USER_STORAGE);
+	private static File[] dir = storage.listFiles();
 	
 	public static void main(String[] args)
 	{
@@ -15,11 +22,12 @@ public class LockedMe
 		} 
 		catch (Exception e) 
 		{
-			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
 		}
 		finally
 		{
 			input.close();
+			System.out.println("Thank you for using the LockedMe App.");
 		}
 	}
 	
@@ -29,8 +37,9 @@ public class LockedMe
 		System.out.println("After the following options, you may input the number corresponding of the option:");
 		System.out.println("1. Option");
 		System.out.println("2. Option");
-		System.out.println("Please type your input of c1hoice: \n");
+		System.out.println("Please type your input of choice: \n");
 		sel = input.nextLine();
+		Arrays.parallelSort(dir);
 		if(sel.startsWith("1") || sel.equalsIgnoreCase("o"))
 		{
 			firstOption();
@@ -44,7 +53,17 @@ public class LockedMe
 	
 	private static void firstOption() throws Exception
 	{
-		System.out.println("FirstOptionSelected");
+		if(dir != null)
+		{
+			for(File f : dir)
+				System.out.println(f.getName());
+		}
+		else
+		{
+			System.out.println("The current repository is empty.\n\n");
+		}
+		System.out.print("\n");
+		mainScreen();
 	}
 	
 	private static void secondOption() throws Exception
