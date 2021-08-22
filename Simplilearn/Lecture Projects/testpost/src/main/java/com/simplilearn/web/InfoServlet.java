6b378e95131c5f5c4c997b024b2b224ba4ce799e
ProgrammedPeinado <hectorpeinado.luis@gmail.com>
2021-08-22 04:16:05 -0400

    Day 1 - Course 3 get/post example

---
+++ Simplilearn/Lecture Projects/testpost/src/main/java/com/simplilearn/web/InfoServlet.java
@@ -0,0 +1,37 @@
+package com.simplilearn.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfoServlet
+ */
+@WebServlet("/postinfo")
+public class InfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("From doPost in response to post");
+	}
+
+}
