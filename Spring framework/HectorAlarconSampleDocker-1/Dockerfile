3c254ced4aeba8cd90eaf479036792810ebe3643
ProgrammedPeinado <hectorpeinado.luis@gmail.com>
2023-07-07 15:17:32 -0400

    Phase 5 - SpringBoot File to be served by Jenkins

---
+++ Spring framework/HectorAlarconSampleDocker-1/Dockerfile
@@ -0,0 +1,4 @@
+From openjdk:17
+EXPOSE 8080
+ADD target/HectorAlarconSampleDocker-1-0.0.1-SNAPSHOT.war HectorAlarconSampleDocker-1-0.0.1-SNAPSHOT.war
+ENTRYPOINT ["java", "-jar","/HectorAlarconSampleDocker-1-0.0.1-SNAPSHOT.war"]
---
+++ Spring framework/HectorAlarconSampleDocker-1/src/main/java/com/finalsample/dockerdemo/HectorAlarconSampleDocker1Application.java
@@ -0,0 +1,13 @@
+package com.finalsample.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HectorAlarconSampleDocker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HectorAlarconSampleDocker1Application.class, args);
+	}
+
+}
